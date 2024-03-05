b09f2a03be49273e47390424beb56d269ae813b6
Maxime Bastien <113992556+Maxime122004@users.noreply.github.com>
2024-03-05 11:31:30 +0100

    gh-pages added

---
+++ react-test/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import Contact from './pages/Blogs';
+
+function App() {
+  <>
+
+    <h1>React Router Demo</h1>
+
+    <BrowserRouter basename="/{frontend-courseproject-Maxime122004}">
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='Blogs' element={<Blogs />} />
+          <Route path='Contact' element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
+}
+
+export default App
---
+++ react-test/src/pages/Blogs.jsx
@@ -0,0 +1,10 @@
+function Blogs () {
+    return (
+        <div>
+            <h2>Blogs</h2>
+            <p>Coming soon...</p>
+        </div>
+    );
+}
+
+export default Blogs;
