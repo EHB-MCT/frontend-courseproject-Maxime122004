9e7abd5d2622b9ae1419486e6a7617d4aefbe50d
Maxime Bastien <113992556+Maxime122004@users.noreply.github.com>
2024-05-14 21:43:21 +0200

    first 3 sprookjes are random and never duplicates

--- react-test/src/pages/AboutUs.jsx
+++ react-test/src/pages/AboutUs.jsx
@@ -1,35 +1,41 @@
 import { Link } from "react-router-dom";
+import DATA from '../data.json';
+import Sprookje from "./Components/Sprookje";
 
 function AboutUs() {
+
+    const filteredSprookjes = [];
+    // let currentNumber = 0;
+
+    const pickRandomSprookje = () => {
+        for (let i = 0; i < 3; i++) {
+            let randomSprookje = Math.floor(Math.random() * DATA.length);
+            if (i > 0) {
+                while (filteredSprookjes.includes(DATA[randomSprookje])) {
+                    randomSprookje = Math.floor(Math.random() * DATA.length);
+                }
+            }
+            filteredSprookjes.push(DATA[randomSprookje])
+        }
+    }
+    pickRandomSprookje();
+
+
     return (
         <main>
             <div>
                 <h1>Populair</h1>
                 <section className="sprookjes">
-                    <Link to='/HansGrietje'>
-                        <img src="images/portaal_pagina/sprookjes_template_image.png"/>
-                        <img src="images/portaal_pagina/hans_en_grietje_tumbnail.jpg" alt="sprookje" />
-                        <h2>Hans en Grietje</h2>
-                        <p>Student</p>
-                        <p>Maxime Bastien</p>
-                        <img src="images/portaal_pagina/arrow.png" alt="arrow" />
-                    </Link>
-                    <Link to='/HansGrietje'>
-                        <img src="images/portaal_pagina/sprookjes_template_image.png"/>
-                        <img src="images/portaal_pagina/hans_en_grietje_tumbnail.jpg" alt="sprookje" />
-                        <h2>Hans en Grietje</h2>
-                        <p>Student</p>
-                        <p>Maxime Bastien</p>
-                        <img src="images/portaal_pagina/arrow.png" alt="arrow" />
-                    </Link>
-                    <Link to='/HansGrietje'>
-                        <img src="images/portaal_pagina/sprookjes_template_image.png"/>
-                        <img src="images/portaal_pagina/hans_en_grietje_tumbnail.jpg" alt="sprookje" />
-                        <h2>Hans en Grietje</h2>
-                        <p>Student</p>
-                        <p>Maxime Bastien</p>
-                        <img src="images/portaal_pagina/arrow.png" alt="arrow" />
-                    </Link>
+                    {filteredSprookjes.map((item) => (
+                        <Sprookje
+                            key={item.id}
+                            url={item.url}
+                            title={item.title}
+                            image={item.image}
+                            student={item.student}
+                            genre={item.genre}
+                        />
+                    ))}
                 </section>
                 <Link to='/Sprookjes' className="loadMore">
                     <img src="images/portaal_pagina/arrow.png" alt="arrow" />
